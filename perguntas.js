b4f0b2be25d1ad7c15963a6695cf097963e6760d
NathieleGabriele <165436797+NathieleGabriele@users.noreply.github.com>
2025-05-12 10:44:47 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,53 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Física',
+    'Qual é a unidade de medida para tensão elétrica?',
+    'Volt (V)'
+) 
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz OI em inglês?',
+    'Oi em ingles é HI (RAI)'
+)
+
+criaCartao(
+    'Química',
+    '',
+    'Oi em ingles é HI (RAI)'
+)
+
+criaCartao(
+    'Lingua Portuguesa',
+    'Como se diz OI em inglês?',
+    'Oi em ingles é HI (RAI)'
+)
+
+criaCartao(
+    'História',
+    'Como se diz OI em inglês?',
+    'Oi em ingles é HI (RAI)'
+)
+
+criaCartao(
+    'Matemática',
+    'Como se diz OI em inglês?',
+    'Oi em ingles é HI (RAI)'
+)
